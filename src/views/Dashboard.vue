0470125f894383984be6b28b5eeff782eedc4c25
jinan <jinan5694@163.com>
2018-11-06 18:17:44 +0800

    test

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,44 +4,119 @@
       <el-button type="primary">{{ $t('ok') }}</el-button>
       <el-button>{{ $t('back') }}</el-button>
     </template>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
-    <h1>dashboard</h1>
+    <BaseTable
+      :columns="columns"
+      :data="tableData"
+      :span-method="spanMethod"
+      border>
+      <!-- slot -->
+    </BaseTable>
+
   </Page>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      tableData: [
+        {
+          id: '1',
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+          rowspan: 3
+        },
+        {
+          id: '1',
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄',
+          rowspan: 0
+        },
+        {
+          id: '1',
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄',
+          rowspan: 0
+        },
+        {
+          id: '2',
+          date: '2016-05-01',
+          name: '张曼玉',
+          address: '上海市普陀区金沙江路 1516 弄',
+          rowspan: 3,
+          colspan: 1
+        },
+        {
+          id: '2',
+          date: '2016-05-02',
+          name: '张曼玉',
+          address: '上海市普陀区金沙江路 1516 弄',
+          rowspan: 0,
+          colspan: 1
+        },
+        {
+          id: '2',
+          date: '2016-05-03',
+          name: '张曼玉',
+          address: '上海市普陀区金沙江路 1516 弄',
+          rowspan: 0,
+          colspan: 1
+        }
+      ],
+      ids: ['1', '2']
+    }
+  },
+  computed: {
+    columns () {
+      return [
+        {
+          label: '姓名',
+          prop: 'name'
+        },
+        {
+          label: '日期',
+          prop: 'date'
+        },
+        {
+          label: '地址',
+          prop: 'address'
+        }
+      ]
+    },
+    data () {
+      return this.tableData.map(item => {
 
+      })
+    }
+  },
+  methods: {
+    spanMethod ({ row, column, rowIndex, columnIndex }) {
+      // debugger
+      console.log(`rowIndex:${rowIndex}, columnIndex:${columnIndex}`)
+      if (columnIndex === 0) {
+        return {
+          rowspan: row.rowspan || 1,
+          colspan: row.colspan || 1
+        }
+      }
+      // if (columnIndex < 2) {
+      //   if (rowIndex % 3 === 0) {
+      //     return {
+      //       rowspan: 3,
+      //       colspan: 1
+      //     }
+      //   } else {
+      //     return {
+      //       rowspan: 0,
+      //       colspan: 1
+      //     }
+      //   }
+      // }
+    }
+  }
 }
 </script>
 
